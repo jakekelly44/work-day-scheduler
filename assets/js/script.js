0429b14302d2b19ba5dbe63f696caba32cd6914d
Jake Kelly <jakekelly@gmail.com>
2020-06-26 10:57:57 -0700

    storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,3 +30,61 @@ const initPlanner = () => {
 }
 
 //Planner Function//
+
+//Local Storage//
+
+const saveNine = document.getElementById('save9');
+const saveNineField = () => {
+    let nineAMText = nineAM.value;
+    localStorage.setItem("nine", nineAMText);
+}
+
+const saveTen = document.getElementById('save10');
+const saveTenField = () => {
+    let tenAMText = tenAM.value;
+    localStorage.setItem("ten", tenAMText);
+}
+
+const saveEleven = document.getElementById('save11');
+const saveElevenField = () => {
+    let elevenAMText = elevenAM.value;
+    localStorage.setItem("eleven", elevenAMText);
+}
+
+const saveTwelve = document.getElementById('save12');
+const saveTwelveField = () => {
+    let twelvePMText = twelvePM.value;
+    localStorage.setItem("twelve", twelvePMText);
+}
+
+const saveOne = document.getElementById('save1');
+const saveOneField = () => {
+    let onePMText = onePM.value;
+    localStorage.setItem("one", onePMText);
+}
+
+const saveTwo = document.getElementById('save2');
+const saveTwoField = () => {
+    let twoPMText = twoPM.value;
+    localStorage.setItem("two", twoPMText);
+}
+
+const saveThree = document.getElementById('save3');
+const saveThreeField = () => {
+    let threePMText = threePM.value;
+    localStorage.setItem("three", threePMText);
+}
+
+const saveFour = document.getElementById('save4');
+const saveFourField = () => {
+    let fourPMText = fourPM.value;
+    localStorage.setItem("four", fourPMText);
+}
+
+const saveFive = document.getElementById('save5');
+const saveFiveField = () => {
+    let fivePMText = fivePM.value;
+    localStorage.setItem("five", fivePMText);
+}
+
+//Local Storage//
